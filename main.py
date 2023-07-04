50c12e97f150b0f37a568e8713c5c20b9de27ac8
Norze <123885505+N0rz3@users.noreply.github.com>
2023-07-04 23:29:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from lib.banner import bann
 def version():
     version = sys.version_info
     
-    if (version > (3, 10)):
+    if (version < (3, 10)):
         print("SpyScraper only works with Python 3.10+.")
         print("[+] Go install the most recent version of python -> https://www.python.org/downloads/")
 
