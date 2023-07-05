a75a55c185b54b80f527e4eb404119f20330f64f
Norze <123885505+N0rz3@users.noreply.github.com>
2023-07-05 12:07:27 +0200

    Update formats.py

--- lib/formats.py
+++ lib/formats.py
@@ -1,5 +1,3 @@
-GLOBAL = r"^(http[s]?:\/\/)?(www\.)?twitter\.com\/[a-zA-Z0-9_]+\/?$"
-
 EMAIL = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
 
 PHONE_NUMBER = r"\b(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})\b"
