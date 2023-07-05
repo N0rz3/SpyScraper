124799a2126e29eb6c39ff7f2a83fbbe1d871fa5
Norze <123885505+N0rz3@users.noreply.github.com>
2023-07-05 19:49:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def version():
     if (version < (3, 10)):
         print("SpyScraper only works with Python 3.10+.")
         print("[+] Go install the most recent version of python -> https://www.python.org/downloads/")
+        exit()
 
 
     from lib.launcher import launch
